7ff0e1ceed911799359006f0bdf009fa91f7458d
crnobog <carskalavra@proton.me>
2024-12-14 20:05:05 +0100

    ❄️

--- prviSemestar-2024/kolokvijum2-2024/funkcije/zadatak103.c
+++ prviSemestar-2024/kolokvijum2-2024/funkcije/zadatak103.c
@@ -1,2 +1,26 @@
 // Саставити рекурзивну функцију која исписује првих n бројева у обрнутом редследу,
 // а затим тестирати функцију за дато n.
+
+#include <stdio.h>
+
+void Stampaj(unsigned int n)
+{
+    if (n == 0)
+        return;
+
+    printf(" %d", n);
+    Stampaj(n - 1); // Функција позиува саму себе и тиме прави`петљу`
+}
+
+int main()
+{
+    int n;
+
+    printf(" n = ");
+    scanf("%d", &n);
+
+    Stampaj(n);
+
+    getchar();
+    return 0;
+}
---
+++ prviSemestar-2024/kolokvijum2-2024/funkcije/zadatak104.c
@@ -0,0 +1,25 @@
+// Саставити рекурзивну функцију која одређује и исписује n - ти члан Фибоначијевог низа,
+// а затим тестирати функцију за дато n.
+// Фибоначијев низ је : f1 = 1, f2 = 1, fi = fi - 1 + fi - 2, i = 3, 4, 5, ...
+
+#include <stdio.h>
+
+int Fib(int n)
+{
+    if ((n == 0) || n == 1)
+        return 1;
+    else
+        return (Fib(n - 1) + Fib(n - 2));
+}
+
+int main()
+{
+    int n;
+    printf(" n = ");
+    scanf("%d", &n);
+
+    printf(" Fib[%d] = %d\n", n, Fib(n - 1));
+
+    getchar();
+    return 0;
+}
