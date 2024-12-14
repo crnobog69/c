be94a1e0d115965dbba0ee0ac6466a1a73fcb844
crnobog <carskalavra@proton.me>
2024-12-14 19:52:30 +0100

    ❄️

--- prviSemestar-2024/kolokvijum2-2024/funkcije/zadatak100.c
+++ prviSemestar-2024/kolokvijum2-2024/funkcije/zadatak100.c
@@ -11,4 +11,28 @@ double Ekspon(double x, int n)
         clan *= x / i;
         ex += clan;
     }
+
+    return ex;
+}
+
+int main()
+{
+    double x, ex, ext, apsgr;
+    int n;
+
+    printf(" x = ");
+    scanf("%lf", &x);
+    printf(" n = ");
+    scanf("%d", &n);
+
+    ex = Ekspon(x, n);
+    ext = exp(x);
+    apsgr = fabs(ex - ext);
+
+    printf("\n Izracunato = %.4f", ex);
+    printf("\n Tacno = %.4f", ext);
+    printf("\n ASPGR = %.4f", apsgr);
+
+    getchar();
+    return 0;
 }
---
+++ prviSemestar-2024/kolokvijum2-2024/funkcije/zadatak101.c
@@ -0,0 +1,23 @@
+// Саставити рекурзивну функцију за одређивање факторијела целог броја,
+// а затим је тестирати у главном програму за унети цео број n и исписати добијени резултат.
+
+#include <stdio.h>
+
+int Faktorijel(int n)
+{
+    if (n == 1)
+        return 1;
+    return n * Faktorijel(n - 1);
+}
+
+int main()
+{
+    int n;
+
+    printf(" n = ");
+    scanf("%d", &n);
+    printf("%d!= % d\n", n, Faktorijel(n));
+
+    getchar();
+    return 0;
+}
